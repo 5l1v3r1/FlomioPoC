cebb5fa60febf4acae5e2e9b7ff8aa13af8b7a50
Keith Burnell <keith@kburnell.com>
2017-04-01 10:07:29 -0500

    initial commit

---
+++ ViewController-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ViewController-Bridging-Header.h
+//  flomio
+//
+//  Created by KEITH BURNELL on 3/30/17.
+//  Copyright © 2017 That Conference. All rights reserved.
+//
+
+#ifndef ViewController_Bridging_Header_h
+#define ViewController_Bridging_Header_h
+
+#import "FmSessionManager.h"
+
+#endif /* ViewController_Bridging_Header_h */
--- flomio/ViewController.swift
+++ flomio/ViewController.swift
@@ -8,16 +8,115 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FmSessionManagerDelegate {
 
+    @IBOutlet weak var btnWriteTag: UIButton!
+    @IBOutlet weak var btnReadTag: UIButton!
+    @IBOutlet weak var btnCheckIn: UIButton!
+    @IBOutlet weak var txtLog: UITextView!
+    
+    var device : FmDevice?
+    var theDeviceId : String!
+    let readerManager : FmSessionManager = FmSessionManager()
+    
+    @IBAction func btnWriteTagClick(_ sender: Any) {
+        writeLog(message: "Write Tag Clicked!");
+        let data = NSUUID().uuidString;
+        readerManager.sendApdu(data, toDevice: theDeviceId);
+        writeLog(message: "Sent APDU: \(data)")
+    }
+    
+    @IBAction func btnReadTagClick(_ sender: Any) {
+        writeLog(message: "Read Tag Clicked!");
+    }
+    
+    @IBAction func btnCheckInClick(_ sender: Any) {
+        writeLog(message: "Check In Clicked!");
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        initNfcHardware();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func writeLog(message: String) {
+        txtLog.text = txtLog.text + "\n" + message;
+    }
+    
+    func didFindTag(withUuid Uuid: String!, fromDevice deviceId: String!, withAtr Atr: String!, withError error: Error!) {
+        //writeLog(message: "didFindTag1");
+        theDeviceId = deviceId
+        writeLog(message: "DEVICE ID: \(theDeviceId)");
+        DispatchQueue.main.async {
+            if let thisUuid = Uuid {
+                self.writeLog(message: "SCANNED - GUID: \(thisUuid)")
+            } else {
+                self.writeLog(message: "Error!")
+            }
+        }
+    }
+    
+    func didFindTag(withData payload: [AnyHashable : Any]!, fromDevice deviceId: String!, withAtr Atr: String!, withError error: Error!) {
+        //writeLog(message: "didFindTag2");
+        DispatchQueue.main.async {
+            let thisDeviceId = deviceId
+            if let thisPayload = payload["Raw Data"] {
+                self.writeLog(message: "Did find payload (raw): \(thisPayload) from Device: \(thisDeviceId)")
+            } else if let ndef = payload["Ndef"] {
+                self.writeLog(message: "Did find payload (ndef): \(ndef) from Device: \(thisDeviceId)")
+            }
+        }
+    }
+    
+    func didUpdateConnectedDevices(_ devices: [Any]!) {
+        //writeLog(message: "didUpdateConnectedDevices");
+        //The list of connected devices was updated
+    }
+    
+    func didChange(_ status: CardStatus, fromDevice device: String!) {
+        //writeLog(message: "didChange");
+        DispatchQueue.main.async {
+            //The card status has entered or left the scan range of the reader
+            // Cardstatus:
+            // 0:CardStatus.notPresent
+            // 1:CardStatus.present
+            // 2:CardStatus.readingData
+        }
+    }
+    
+    func didReceiveReaderError(_ error: Error!) {
+        //writeLog(message: "didReceiveReaderError");
+        DispatchQueue.main.async {
+            self.writeLog(message: "Error!")
+        }
+    }
+    
+    func inactive() {
+        writeLog(message: "App Inactive")
+    }
+    
+    func active() {
+        writeLog(message: "App Activated")
+    }
+    
+    func initNfcHardware() {
+        readerManager.selectedDeviceType = DeviceType.flojackMsr;
+        readerManager.delegate = self
+        readerManager.specificDeviceId = nil;
+        let configurationDictionary : [String : Any] =
+            ["Scan Sound" : 1,
+             "Scan Period" : 1000,
+             "Reader State" : ReaderStateType.readData.hashValue, //readData for NDEF
+             "Allow Multiconnect" : false
+        ]
+        readerManager.setConfiguration(configurationDictionary)
+        readerManager.createReaders();
+        writeLog(message: "NFC Hardware Initialized");
     }
 
 
